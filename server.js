eeb5654607b1efe366930f3d3575205009c6be6e
Gomberg2024 <gomberg2010@hotmail.com>
2024-12-31 18:20:27 -0300

    a

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-// Middleware para servir arquivos estáticos da pasta "public"
+// Middleware para interpretar requisições com JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rotas específicas para cada página
+// Rotas
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -20,20 +25,7 @@ app.get('/cadastro-processos', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'cadastro-processos.html'));
 });
 
-app.get('/cadastro-clientes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'cadastro-clientes.html'));
-});
-
-app.get('/logout', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'logout.html'));
-});
-
-// Tratamento de erros 404 para páginas não encontradas
-app.use((req, res) => {
-    res.status(404).send('Página não encontrada');
-});
-
-// Inicializa o servidor
+// Escutando na porta especificada
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
