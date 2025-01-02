1b5006e22d1f14ca69af99b2d439d3547cf800c6
Gomberg2024 <gomberg2010@hotmail.com>
2025-01-02 15:59:58 -0300

    A

--- server.js
+++ server.js
@@ -12,28 +12,17 @@ app.use(bodyParser.json());
 // Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota principal (index.html)
+// Rotas
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Rota para área administrativa
-app.get('/area-administrativa.html', (req, res) => {
+app.get('/area-administrativa', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'area-administrativa.html'));
 });
 
-// Rota para cadastro de processos
-app.get('/cadastro-processos.html', (req, res) => {
+app.get('/cadastro-processos', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'cadastro-processos.html'));
-});
-
-// Outras rotas podem ser adicionadas aqui se necessário
-app.get('/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-
-app.get('/formulario.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'formulario.html'));
 });
 
 // Escutando na porta especificada
